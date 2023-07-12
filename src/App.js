d75e10344b1fa55f0ad7f1d69d96bd65e929e960
klimov <AI.Klimov@mit.ooo>
2023-07-12 18:52:13 +0300

    u8 fin

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 
-function App(props){
+export default function App(props){
 
-  const Test = (a) =>{
-    console.log(a)
-  }
+  const[calls, setCalls] = useState(0) 
+  const[red, setRed] = useState(false)
+  const sttl = red===false? "aquamarine" : "red";
+  const slf =  useMemo((e)=>slowFunction(calls),[calls]);
 
-  const[state, setState] = useState(props.counter)  
-  const[test, setTest] = useState(() => Test('abcaaac'))
-  //console.log(useState(5))
-  
-  const onChangeCounter = (e) => {
-    setState(state => state + e)
-  }
-
-  const onResetCounter = ( ) => {
-    setState(state => 0)
-  }
-
-  const onRandomCounter = (min, max) => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    setState(Math.floor(Math.random()*(max-min+1)+min))
-    
-  }
+  const myComponentStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: `${sttl}`,
+ }
+ 
+  //<button onClick={() => onRandomCounter(20,30)}>RND</button> 
+  // <button onClick={(e)=>setRed(red===true ? false : true)}>Colour change</button>
 
    return (
-    <div className="App">
-      <div className='counter'>{state}</div>
-      <div className='controls'>
-        <button onClick={() => onChangeCounter(1)}>INC</button>
-        <button onClick={() => onChangeCounter(-1)}>DEC</button>
-        <button onClick={() => onResetCounter()}>RESET</button>
-        <button onClick={() => onRandomCounter(20,30)}>RND</button>       
+    <div style={myComponentStyle} className="App">
+      <div className="CallsLable">REST CALLS: {calls}</div>
+      <div className="controls">
+        <button onClick={(e)=>setCalls(calls+1)}>START CALL</button>
+        <button onClick={(e)=>setRed(red===true ? false : true)} >Colour change</button>
       </div>
     </div>
   );   
 }
 
-export default App;
+function slowFunction(num ){
+  for(let i = 0; i <= 2000000000; i++ ){}
+  //console.log("sttl "+sttl)
+  return console.log("calling slow function " +num*2)
+}
