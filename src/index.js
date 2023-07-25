67ede693f175a913b3b05d7d1d8dde80dc9bcdec
klimov <AI.Klimov@mit.ooo>
2023-07-25 15:00:33 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+  clearForageF as clearF
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+import { Children } from "react";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+ 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+    children:[
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "clear",
+        action: clearF,
+      }
+    ],
+
+  },
+
+  //this way it is going to be a complitely separate page
+  //{
+  //  path: "contacts/:contactId",
+  //  element: <Contact />,
+  //},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App counter ={0}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
